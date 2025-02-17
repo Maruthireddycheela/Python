38d8323a0627f01cfdefa4a7af0bc6264defdacd
Maruthireddycheela <cheelamaruthireddy@gmail.com>
2025-02-17 23:16:00 +0530

    Revers_string

---
+++ smart_soc/1.py
@@ -0,0 +1,31 @@
+# Write a program for reverse string 
+
+"""
+We need to take a string
+
+Hello --> is a string
+
+reverse a string
+olleH  --> reversed string
+
+"""
+
+# 1st method
+
+def reverse_string(s):
+    reversed_string = " "
+    for char in s:
+        reversed_string = char + reversed_string
+    return reversed_string
+
+reversed_string = "Hello"
+print("reverse_string:", reverse_string(reversed_string))
+
+
+
+# 2nd method
+
+def reverse_string(s):
+    return s[::-1]
+rev = "Hello"
+print("reversed_string:", reverse_string(rev))
