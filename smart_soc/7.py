946a7baa44a69103344b2f171bbebebe926cccbf
Maruthireddycheela <cheelamaruthireddy@gmail.com>
2025-02-18 23:44:18 +0530

    separate characters

---
+++ smart_soc/7.py
@@ -0,0 +1,7 @@
+# write a program for given list sunday is the string and separate it
+
+string = "sunday"
+
+char_list = list(string) # Convert the string to a list of characters
+
+print(char_list)
